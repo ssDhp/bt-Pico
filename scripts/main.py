700c160c85fa6080a42791da20f4ce71ceb2b826
Axel Windows <110320550+ssDhp@users.noreply.github.com>
2022-12-18 13:53:03 +0530

    Update file structure

--- scripts/main.py
+++ scripts/main.py
@@ -1,15 +1,16 @@
-from machine import Pin, UART
 import utime
+from machine import Pin, UART
+from micropyGPS import MicropyGPS
+
 gpsModule = UART(0, baudrate=9600, tx=Pin(16), rx=Pin(17))
 print(gpsModule)
 
 buff = bytearray(255)
-
+gps = MicropyGPS()
 
 while True:
-    print('Trying')
     gpsModule.readline()
     buff = str(gpsModule.readline())
     parts = buff.split(',')
     print(buff)
-    utime.sleep_ms(500)
+    utime.sleep_ms(1000)
