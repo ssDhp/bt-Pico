1a5684f9aa41e3b6cfb2f061cb1e4dbbe38b040e
ssDhp@Zen <110320550+ssDhp@users.noreply.github.com>
2022-12-20 12:55:40 +0530

    GPS should be working

--- scripts/main.py
+++ scripts/main.py
@@ -8,7 +8,21 @@ print(gpsModule)
 gps = MicropyGPS()
 
 while True:
-    while char := gpsModule.read(1):
-        if status := gps.update(char):
-            print()
-    utime.sleep_ms(1000)
+    while gpsModule.any():
+        # Init a byte array to store serial data
+        frame = bytearray(1024)
+        # Store the read data into byte array
+        gpsModule.readinto(frame)
+        # Decode bytearray into string
+        try:
+            data = frame.decode()
+            for line in data.splitlines():
+                if line[3:6] in ['RMC', 'GGA']:
+                    # Update gps data
+                    for x in line:
+                        gps.update(x)
+                    print(gps.latitude_string(), ",", gps.longitude_string())
+
+        except UnicodeError:
+            # Soemtimes noise is read which causes UnicodeError during decoding
+            print(frame)
