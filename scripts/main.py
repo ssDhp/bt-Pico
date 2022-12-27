7e3efbef69a765cb0e99f6d9e37ffbcf85a4977e
Axel Windows <110320550+ssDhp@users.noreply.github.com>
2022-12-27 17:58:54 +0530

    Fixed timestamp bug

--- scripts/main.py
+++ scripts/main.py
@@ -1,4 +1,5 @@
 import utime
+import time
 from machine import Pin, UART
 from NMEA import NMEAparser
 from SIM800L import Modem
@@ -17,27 +18,36 @@ print(f'\nModem IP address: "{simModule.get_ip_addr()}"')
 
 while True:
     if gpsModule.any():
+        # Init a byte array to store serial data
+        frame = bytearray(1024)
+        # Store the read data into byte array
+        gpsModule.readinto(frame)
+        # Decode bytearray into string
         try:
-            if staus := gpsParserObject.update((gpsModule.read(1)).decode("ASCII")):
-                picoLed.value(1)
-                print("\nNow running GET command")
-                response = simModule.http_request(
-                    getUrl(
-                        gpsParserObject.lat,
-                        gpsParserObject.lng,
-                        gpsParserObject.utc_time,
-                    ),
-                    "GET",
-                )
-                print("Response status code:", response.status_code)
-                print("Response content:", response.content)
-                picoLed.value(0)
-                # Blink if request was sucessfull
-                if response.status_code == 200:
-                    utime.sleep(0.3)
-                    picoLed.toggle()
-                    utime.sleep(0.3)
-                    picoLed.toggle()
-                utime.sleep(5)
+            data = frame.decode()
+            # Update gps data
+            for x in data:
+                gpsParserObject.update(x)
         except UnicodeError:
             pass
+        print(f"\nLat: {gpsParserObject.lat}, Long: {gpsParserObject.lng}, UTC: {gpsParserObject.utc_time}")
+        picoLed.value(1)
+        try:
+            print("Now running GET command")
+            t = time.ticks_ms()
+
+            response = simModule.http_request(getUrl(gpsParserObject.lat, gpsParserObject.lng, gpsParserObject.utc_time), "GET",)
+            print(f"Time taken to make the  request:{time.ticks_diff(time.ticks_ms(),t)/1000} sec")
+            print("Response status code:", response.status_code)
+            print("Response content:", response.content)
+        except Exception:
+            print("Request Failed!")
+            pass
+        picoLed.value(0)
+        # Blink if request was sucessfull
+        if response.status_code == 200:
+            utime.sleep(0.3)
+            picoLed.toggle()
+            utime.sleep(0.3)
+            picoLed.toggle()
+        # utime.sleep(5)
