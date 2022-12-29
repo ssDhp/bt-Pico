c04a63b6f49393930a8e40c3cfe2d99aeaaa3ab3
Lakshyajeet Jalal <lakshyajeetjalal@gmail.com>
2022-12-29 13:15:57 +0530

    new channel

--- scripts/main.py
+++ scripts/main.py
@@ -20,24 +20,36 @@ while True:
     if gpsModule.any():
         try:
             if staus := gpsParserObject.update((gpsModule.read(1)).decode("ASCII")):
-                print(f"\nLat: {gpsParserObject.lat}, Long: {gpsParserObject.lng}, UTC: {gpsParserObject.utc_time}")
-                try:
-                    picoLed.value(1)
-                    print("Now making HTTP GET request")
-                    t = time.ticks_ms()
-                    response = simModule.http_request(getUrl(gpsParserObject.lat, gpsParserObject.lng, gpsParserObject.utc_time,), "GET")
-                    print(f"Time taken to make the request: {time.ticks_diff(time.ticks_ms(),t)/1000} sec")
-                    print("Response code:", response.status_code)
-                    print("Response:", response.content)
-                    # Blink if request was sucessfull
-                    if response.status_code == 200:
-                        utime.sleep(0.3)
-                        picoLed.toggle()
-                        utime.sleep(0.3)
-                        picoLed.toggle()
-                except Exception as error:
-                    print("Request Failed!", error)
-                    pass
+                if gpsParserObject.lat and gpsParserObject.lng:
+                    print(
+                        f"\nLat: {gpsParserObject.lat}, Long: {gpsParserObject.lng}, UTC: {gpsParserObject.utc_time}"
+                    )
+                    try:
+                        picoLed.value(1)
+                        print("Now making HTTP GET request")
+                        t = time.ticks_ms()
+                        response = simModule.http_request(
+                            getUrl(
+                                gpsParserObject.lat,
+                                gpsParserObject.lng,
+                                gpsParserObject.utc_time,
+                            ),
+                            "GET",
+                        )
+                        print(
+                            f"Time taken to make the request: {time.ticks_diff(time.ticks_ms(),t)/1000} sec"
+                        )
+                        print("Response code:", response.status_code)
+                        print("Response:", response.content)
+                        # Blink if request was sucessfull
+                        if response.status_code == 200:
+                            utime.sleep(0.3)
+                            picoLed.toggle()
+                            utime.sleep(0.3)
+                            picoLed.toggle()
+                    except Exception as error:
+                        print("Request Failed!", error)
+                        pass
         except UnicodeError:
             pass
         picoLed.value(0)
