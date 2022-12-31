25d74fbbedf765dc4cbb8d017df72b93cc67e017
Lakshyajeet Jalal <lakshyajeetjalal@gmail.com>
2022-12-31 14:23:15 +0530

    signal -> publish reverted

--- scripts/helper.py
+++ scripts/helper.py
@@ -28,4 +28,4 @@ channel = "bus_" + env.id.busNo
 
 def getUrl(lat: float, lng: float, utc: float):
     payload = f"%7B%22lat%22%3A%20{lat}%2C%0A%22lng%22%3A%20{lng}%2C%0A%22utc%22%3A%20{utc}%0A%7D"
-    return f"http://ps.pndsn.com/signal/{env.pubnub.publishKey}/{env.pubnub.subscribeKey}/0/{channel}/0/{payload}?uuid={env.id.uuid}"
+    return f"http://ps.pndsn.com/publish/{env.pubnub.publishKey}/{env.pubnub.subscribeKey}/0/{channel}/0/{payload}?uuid={env.id.uuid}"
